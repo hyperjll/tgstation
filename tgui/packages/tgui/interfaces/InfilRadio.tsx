0613d59f0d492a595cb81b36f8354299a06a250c
hyperjll <jllhyper@gmail.com>
2024-11-16 19:09:22 -0500

    Testing

--- tgui/packages/tgui/interfaces/InfilRadio.tsx
+++ tgui/packages/tgui/interfaces/InfilRadio.tsx
@@ -1,19 +1,19 @@
 import { useBackend } from '../backend';
 import { 
- Box,
- Button,
- Section,
+  Box,
+  Button,
+  Section,
 } from '../components';
 import { Window } from '../layouts';
 
 export const InfilRadio = (props) => {
   const { act, data } = useBackend();
-  const { check, completed, final } = data;
+  const { check, completed } = data;
   return (
     <Window width={360} height={130} theme="hackerman">
       <Window.Content>
         <Section>
-          {(completed && <Box> {final} </Box>) ||
+          {(completed) ||
             (data.check
               ? 'Your work has satisfied our investors.'
               : 'Prove your worth by completing an objective.')}
